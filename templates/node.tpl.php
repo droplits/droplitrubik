fee3facf1c0ab75f9bf0bea63bb9d2e458024c23
Scott Blackburn <scott@droplits.com>
2009-12-30 16:30:50 -0600

    from 1.1 to 1.2

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -3,20 +3,6 @@
 <?php if (!empty($pre_object)) print $pre_object ?>
 
 <div <?php if (!empty($attr)) print drupal_attributes($attr) ?>>
-  <?php if ($layout && !empty($submitted) || !empty($links)): ?>
-    <div class='column-side'><div class='column-wrapper'>
-  <?php endif; ?>
-
-  <?php if (!empty($submitted)): ?>
-    <div class='<?php print $hook ?>-submitted clear-block'><?php print $submitted ?></div>
-  <?php endif; ?>
-  <?php if (!empty($links)): ?>
-    <div class='<?php print $hook ?>-links clear-block'><?php print $links ?></div>
-  <?php endif; ?>
-
-  <?php if ($layout && !empty($submitted) || !empty($links)): ?>
-    </div></div>
-  <?php endif; ?>
 
   <?php if ($layout): ?>
     <div class='column-main'><div class='column-wrapper'>
@@ -38,6 +24,22 @@
   <?php if ($layout): ?>
     </div></div>
   <?php endif; ?>
+
+  <?php if ($layout && !empty($submitted) || !empty($links)): ?>
+    <div class='column-side'><div class='column-wrapper'>
+  <?php endif; ?>
+
+  <?php if (!empty($submitted)): ?>
+    <div class='<?php print $hook ?>-submitted clear-block'><?php print $submitted ?></div>
+  <?php endif; ?>
+  <?php if (!empty($links)): ?>
+    <div class='<?php print $hook ?>-links clear-block'><?php print $links ?></div>
+  <?php endif; ?>
+
+  <?php if ($layout && !empty($submitted) || !empty($links)): ?>
+    </div></div>
+  <?php endif; ?>
+
 </div>
 
 <?php if (!empty($post_object)) print $post_object ?>
